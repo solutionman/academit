0ffb8afae6e3c54992175140bf4c8658526f81bd
lom <diskedge.yandex.ru>
2016-10-20 22:07:08 +0700

    Back to basic.

--- Range/src/academit/artamonov2016/range/main/MainRange.java
+++ Range/src/academit/artamonov2016/range/main/MainRange.java
@@ -16,45 +16,30 @@ public class MainRange {
         System.out.print("And second number of first interval: ");
         double number2 = scanner.nextDouble();
 
-        System.out.printf("First interval %.2f - %.2f%n", number1, number2);
+        Range firstInterval = new Range(number1, number2);
+
+        System.out.printf("First interval %.2f - %.2f%n", firstInterval.getFrom1(), firstInterval.getFrom2());
 
         System.out.print("And the number for checking in first interval: ");
         double numberForChecking = scanner.nextDouble();
 
-        System.out.print("Now enter the first number for second interval: ");
-        double secondInterval1 = scanner.nextDouble();
-
-        System.out.print("And second number for second interval: ");
-        double secondInterval2 = scanner.nextDouble();
-
-        System.out.printf("Second interval %.2f - %.2f%n", secondInterval1, secondInterval2);
-
-        Range range = new Range(number1, number2, secondInterval1, secondInterval2);
-        if (range.isInside(numberForChecking)) {
+        if (firstInterval.isInside(numberForChecking)) {
             System.out.printf("The number %.2f is between %.2f - %.2f%n", numberForChecking, number1, number2);
         } else {
             System.out.printf("The number %.2f is outside of interval %.2f - %.2f%n", numberForChecking, number1, number2);
         }
 
-        System.out.printf("The distance between %.2f - %.2f is %.2f%n", number1, number2, range.calcLengthOfInterval());
+        System.out.printf("The distance between %.2f - %.2f is %.2f%n", number1, number2, firstInterval.getLength());
 
-        range.calcIntersectionOfIntervals();
-        System.out.printf("The intersection of %.2f - %.2f  and  %.2f - %.2f  is  %.2f - %.2f%n", number1, number2, secondInterval1, secondInterval2,  range.intersectLesserNumber,range.intersectBiggerNumber);
+        System.out.print("Now enter the first number for second interval: ");
+        double number3 = scanner.nextDouble();
 
-        range.calcUnionIntervals();
-        if(range.unionSecondIntervalLesserNumber == range.unionSecondIntervalBiggerNumber) {
-            System.out.printf("The union of intervals is %.2f - %.2f%n", range.unionFirstIntervalLesserNumber, range.unionFirstIntervalBiggerNumber);
-        } else {
-            System.out.printf("The union of intervals is %.2f - %.2f  and  %.2f - %.2f%n", range.unionFirstIntervalLesserNumber, range.unionFirstIntervalBiggerNumber, range.unionSecondIntervalLesserNumber, range.unionSecondIntervalBiggerNumber);
-        }
+        System.out.print("And second number for second interval: ");
+        double number4 = scanner.nextDouble();
 
-        range.calcFirstMinusSecond();
-        if(range.firstMinusSecondLesserNumber == range.firstMinusSecondBigerNumber){
-            System.out.println("The first interval minus second: 0");
-        } else if(range.firstMinusSecondLesserNumber2 == range.firstMinusSecondBiggerNumber2){
-            System.out.printf("The first interval minus second: %.2f - %.2f", range.firstMinusSecondLesserNumber, range.firstMinusSecondBigerNumber);
-        } else {
-            System.out.printf("The first interval minus second: %.2f - %.2f  and %.2f - %.2f", range.firstMinusSecondLesserNumber, range.firstMinusSecondBigerNumber, range.firstMinusSecondLesserNumber2, range.firstMinusSecondBiggerNumber2);
-        }
+        Range secondInterval = new Range (number3, number4);
+
+        System.out.printf("Second interval %.2f - %.2f%n", secondInterval.getFrom1(), secondInterval.getFrom2());
+
     }
 }
--- Range2/src/academit/artamonov2016/range/logic/IntervalLength.java
+++
@@ -1,7 +0,0 @@
-package academit.artamonov2016.range.logic;
-
-public class IntervalLength {
-    public static double calcLengthOfInterval (double firstNumber, double secondNumber){
-        return Math.abs(Math.abs(firstNumber) - Math.abs(secondNumber));
-    }
-}
