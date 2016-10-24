d01c4485670c3db19e2d5fc4de6172552b8290d4
lom <diskedge.yandex.ru>
2016-10-24 22:37:04 +0700

    Shapes - constructor for square and triangle

--- Shapes/src/shapes/artamonov2016/MainShapes.java
+++ Shapes/src/shapes/artamonov2016/MainShapes.java
@@ -3,5 +3,7 @@
 package shapes.artamonov2016;
 
 public class MainShapes {
+    public static void main(String[] args){
 
+    }
 }
--- Shapes/src/shapes/artamonov2016/Shapes.java
+++ Shapes/src/shapes/artamonov2016/Shapes.java
@@ -2,4 +2,34 @@ package shapes.artamonov2016;
 
 
 public class Shapes {
+
+    private double x1;
+    private double x2;
+    private double x3;
+    private double y1;
+    private double y2;
+    private double y3;
+    private double width;
+    private double height;
+
+    // Square
+    public Shapes(double sideLength){
+        this.width = sideLength;
+    }
+
+    public Shapes (double x1, double y1, double x2, double y2, double x3, double y3){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
+        this.width = Math.max(Math.max(x1, x2), Math.max(x2, x3)) - Math.min(Math.min(x1, x2), Math.min(x2, x3));
+        this.height = Math.max(Math.max(y1, y2), Math.max(y2, y3)) - Math.min(Math.min(y1, y2), Math.min(y2, y3));
+    }
+
+    public double getWidth(){
+
+        return width;
+    }
 }
