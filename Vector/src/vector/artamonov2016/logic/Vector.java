9d1b8ec481e0ef87b21fe546b7f46e4baf224ca7
lom <diskedge.yandex.ru>
2016-10-29 18:02:23 +0700

    Trying to understand what to do with Vector.

---
+++ Vector/src/vector/artamonov2016/logic/Vector.java
@@ -0,0 +1,14 @@
+package vector.artamonov2016.logic;
+
+public class Vector {
+    public double[] components;
+
+    public Vector(double[] n){
+        this.components = n;
+    }
+/*
+    public Vector(Vector){
+
+    }
+*/
+}
---
+++ Vector/src/vector/artamonov2016/main/MainVector.java
@@ -0,0 +1,8 @@
+package vector.artamonov2016.main;
+
+
+public class MainVector {
+    public static void main(String[] args){
+
+    }
+}
