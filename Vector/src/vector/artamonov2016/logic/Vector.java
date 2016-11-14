1dfadb388ef5d004801f16292c4e0863db36efde
lom <diskedge@yandex.ru>
2016-11-14 10:05:27 +0700

    Vector, created all constructors, except copy of Vector

--- Vector/src/vector/artamonov2016/logic/Vector.java
+++ Vector/src/vector/artamonov2016/logic/Vector.java
@@ -3,12 +3,33 @@ package vector.artamonov2016.logic;
 public class Vector {
     public double[] components;
 
-    public Vector(double[] n){
-        this.components = n;
+    public Vector(double n){
+        for(int i = 0;  i < n; ++i ){
+            this.components[i] = 0;
+        }
+
     }
-/*
+
+    // do not understand how to copy
+    /*
     public Vector(Vector){
 
     }
-*/
+    */
+
+    public Vector(double[] arrayOfVectors){
+        for(int i = 0; i < arrayOfVectors.length; ++i){
+            this.components[i] = arrayOfVectors[i];
+        }
+    }
+
+    public Vector(int n, double[] arrayOfVectors){
+        for(int i = 0; i < n; ++i){
+            if(i <= arrayOfVectors.length) {
+                this.components[i] = arrayOfVectors[i];
+            } else{
+                this.components[i] = 0;
+            }
+        }
+    }
 }
