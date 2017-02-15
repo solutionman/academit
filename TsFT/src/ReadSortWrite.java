e675aee464969df0859d59cd9a65b62ef0ee9c47
lom <diskedge@yandex.ru>
2017-02-15 09:39:20 +0700

    still working on sorting array in TsFT

--- TsFT/src/ReadSortWrite.java
+++ TsFT/src/ReadSortWrite.java
@@ -26,14 +26,24 @@ public class ReadSortWrite {
     public static void sortArray (ArrayList<String> arrayForFile){
         for (int i = 0; i < arrayForFile.size(); ++i){
             for(int j = i; j < arrayForFile.size(); ++j){
-               // if(arrayForFile(i) > arrayForFile(j))
+               if((arrayForFile.get(i)).compareTo(arrayForFile.get(j)) > 0){
+                   String temp = arrayForFile.get(i);
+                   arrayForFile.set(i, arrayForFile.get(j));
+                   arrayForFile.set(j, temp);
+                   //arrayForFile.set(i, arrayForFile.get(j)) = arrayForFile.get(j);
+                   //arrayForFile.get(j) = temp;
+               }
             }
         }
     }
 
     public static void writeToFile(ArrayList<String> sortedFile){
         try(PrintWriter out = new PrintWriter("sortedFile.txt")){
-            out.write("hello");
+            for(int i = 0; i < sortedFile.size(); ++i){
+                out.write(sortedFile.get(i));
+                out.write(" ");
+            }
+            //out.write("hello");
         } catch (Exception e){
             System.out.println("Something goes wrong.");
         }
