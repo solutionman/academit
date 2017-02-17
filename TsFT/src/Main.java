48058d57ff376fdd1fa7d77abd2efbcccba80e2f
lom <diskedge@yandex.ru>
2017-02-17 08:45:54 +0700

    works ascending and descending with integers

--- TsFT/src/Main.java
+++ TsFT/src/Main.java
@@ -4,16 +4,25 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the name of the file (with extension .txt). ");
         String nameOfFile = scanner.next();
-        System.out.println("Enter the type of sorting:  a (ascending) d (decreasing)");
+        System.out.println("Enter the type of sorting:  a (ascending) d (descending)");
         String typeOfSorting = scanner.next();
 
         ArrayList<String> arrayFromFile = ReadSortWrite.readToArray(nameOfFile);
 
-        ReadSortWrite.sortArray(arrayFromFile);
+        if (typeOfSorting.equals("a")) {
+            ReadSortWrite.sortArrayAscending(arrayFromFile);
+            System.out.println("File sorted by ascending.");
+        } else if (typeOfSorting.equals("d")) {
+            ReadSortWrite.sortArrayDescending(arrayFromFile);
+            System.out.println("File sorted by descending.");
+        } else {
+            System.out.println("You should type \"a\" or \"d\", file is not sorted.");
+            return;
+        }
 
         ReadSortWrite.writeToFile(arrayFromFile);
 
--- TsFT/src/ReadSortWrite.java
+++ TsFT/src/ReadSortWrite.java
@@ -7,52 +7,56 @@ import java.util.ArrayList;
 
 public class ReadSortWrite {
 
-    public static ArrayList<String> readToArray (String nameOfFile){
+    public static ArrayList<String> readToArray(String nameOfFile) {
         ArrayList<String> arrayForFile = new ArrayList();
         try (
                 BufferedReader in = new BufferedReader(new FileReader(nameOfFile))
         ) {
             String line;
-            while((line = in.readLine()) != null){
+            while ((line = in.readLine()) != null) {
                 arrayForFile.add(line);
             }
-        } catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Something goes wrong. (Seems like there's no such file)");
         }
 
         return arrayForFile;
     }
 
-    public static void sortArray (ArrayList<String> arrayForFile){
-        for (int i = 0; i < arrayForFile.size(); ++i){
-            for(int j = i; j < arrayForFile.size(); ++j){
+    public static void sortArrayAscending(ArrayList<String> arrayForFile) {
+        for (int i = 0; i < arrayForFile.size(); ++i) {
+            for (int j = i; j < arrayForFile.size(); ++j) {
                 Integer firstNumber = Integer.parseInt(arrayForFile.get(i));
                 Integer secondNumber = Integer.parseInt(arrayForFile.get(j));
-               if(firstNumber > secondNumber){
-                   String temp = arrayForFile.get(i);
-                   arrayForFile.set(i, arrayForFile.get(j));
-                   arrayForFile.set(j, temp);
-                   //arrayForFile.set(i, arrayForFile.get(j)) = arrayForFile.get(j);
-                   //arrayForFile.get(j) = temp;
-               } else {
-                   //for testing
-                   // it works
-                   //String temp = arrayForFile.get(i);
-                   //arrayForFile.set(i, arrayForFile.get(j));
-                   //arrayForFile.set(j, temp);
-               }
+                if (firstNumber > secondNumber) {
+                    String temp = arrayForFile.get(i);
+                    arrayForFile.set(i, arrayForFile.get(j));
+                    arrayForFile.set(j, temp);
+                }
             }
         }
     }
 
-    public static void writeToFile(ArrayList<String> sortedFile){
-        try(PrintWriter out = new PrintWriter("sortedFile.txt")){
-            for(int i = 0; i < sortedFile.size(); ++i){
-                out.write(sortedFile.get(i));
-                out.write(" ");
+    public static void sortArrayDescending(ArrayList<String> arrayForFile) {
+        for (int i = 0; i < arrayForFile.size(); ++i) {
+            for (int j = i; j < arrayForFile.size(); ++j) {
+                Integer firstNumber = Integer.parseInt(arrayForFile.get(i));
+                Integer secondNumber = Integer.parseInt(arrayForFile.get(j));
+                if (firstNumber < secondNumber) {
+                    String temp = arrayForFile.get(i);
+                    arrayForFile.set(i, arrayForFile.get(j));
+                    arrayForFile.set(j, temp);
+                }
             }
-            //out.write("hello");
-        } catch (Exception e){
+        }
+    }
+
+    public static void writeToFile(ArrayList<String> sortedFile) {
+        try (PrintWriter out = new PrintWriter("sortedFile.txt")) {
+            for (int i = 0; i < sortedFile.size(); ++i) {
+                out.write(sortedFile.get(i) + "\n");
+            }
+        } catch (Exception e) {
             System.out.println("Something goes wrong.");
         }
     }
