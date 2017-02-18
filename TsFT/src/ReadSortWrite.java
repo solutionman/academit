3ab8d084e1e03c616e70bc4691e7eafb5d220653
SolutionMan <solutionman@inbox.ru>
2017-02-18 10:57:08 +0600

    Everything works

--- TsFT/src/ReadSortWrite.java
+++ TsFT/src/ReadSortWrite.java
@@ -26,12 +26,26 @@ public class ReadSortWrite {
     public static void sortArrayAscending(ArrayList<String> arrayForFile) {
         for (int i = 0; i < arrayForFile.size(); ++i) {
             for (int j = i; j < arrayForFile.size(); ++j) {
-                Integer firstNumber = Integer.parseInt(arrayForFile.get(i));
-                Integer secondNumber = Integer.parseInt(arrayForFile.get(j));
-                if (firstNumber > secondNumber) {
-                    String temp = arrayForFile.get(i);
-                    arrayForFile.set(i, arrayForFile.get(j));
-                    arrayForFile.set(j, temp);
+                char check = arrayForFile.get(i).charAt(0);
+                if (Character.isDigit(check)) {
+                    Integer firstNumber = Integer.parseInt(arrayForFile.get(i));
+                    Integer secondNumber = Integer.parseInt(arrayForFile.get(j));
+                    if (firstNumber > secondNumber) {
+                        String temp = arrayForFile.get(i);
+                        arrayForFile.set(i, arrayForFile.get(j));
+                        arrayForFile.set(j, temp);
+                    }
+                } else if (Character.isLetter(check)) {
+                    Character firstChar = arrayForFile.get(i).charAt(0);
+                    Character secondChar = arrayForFile.get(j).charAt(0);
+                    if (firstChar > secondChar) {
+                        String temp = arrayForFile.get(i);
+                        arrayForFile.set(i, arrayForFile.get(j));
+                        arrayForFile.set(j, temp);
+                    }
+                } else {
+                    System.out.println("File contains wrong symbols.");
+                    return;
                 }
             }
         }
@@ -40,12 +54,25 @@ public class ReadSortWrite {
     public static void sortArrayDescending(ArrayList<String> arrayForFile) {
         for (int i = 0; i < arrayForFile.size(); ++i) {
             for (int j = i; j < arrayForFile.size(); ++j) {
-                Integer firstNumber = Integer.parseInt(arrayForFile.get(i));
-                Integer secondNumber = Integer.parseInt(arrayForFile.get(j));
-                if (firstNumber < secondNumber) {
-                    String temp = arrayForFile.get(i);
-                    arrayForFile.set(i, arrayForFile.get(j));
-                    arrayForFile.set(j, temp);
+                char check = arrayForFile.get(i).charAt(0);
+                if (Character.isDigit(check)) {
+                    Integer firstNumber = Integer.parseInt(arrayForFile.get(i));
+                    Integer secondNumber = Integer.parseInt(arrayForFile.get(j));
+                    if (firstNumber < secondNumber) {
+                        String temp = arrayForFile.get(i);
+                        arrayForFile.set(i, arrayForFile.get(j));
+                        arrayForFile.set(j, temp);
+                    }
+                } else if (Character.isLetter(check)) {
+                    Character firstChar = arrayForFile.get(i).charAt(0);
+                    Character secondChar = arrayForFile.get(i).charAt(0);
+                    if (firstChar < secondChar) {
+                        String temp = arrayForFile.get(i);
+                        arrayForFile.set(i, arrayForFile.get(j));
+                        arrayForFile.set(j, temp);
+                    }
+                } else {
+                    System.out.println("File contains wrong symbols.");
                 }
             }
         }
