39662d687e3db872ace64f4bbb6139c575a5317b
lom <diskedge@yandex.ru>
2017-02-28 09:21:22 +0700

    -a -d

--- TsFT/readme.txt
+++ TsFT/readme.txt
@@ -6,19 +6,19 @@ javac academit/artamonov2017/tsft/Main.java
 
 После этого запускаем саму программу с параметрами:
 
-java academit/artamonov2017/tsft/Main a file.txt
+java academit/artamonov2017/tsft/Main -a file.txt
 
 где параметры:
 file.txt  -название файла с расширением
-a  - сортировка по возрастанию
-b  - сортировка по убыванию
+-a  - сортировка по возрастанию
+-b  - сортировка по убыванию
 
 сортированный файл находится в файле sortedFile.txt
 
 
 например команда:
 
-java academit/artamonov2017/tsft/Main a digits.txt
+java academit/artamonov2017/tsft/Main -a digits.txt
 
 сортирует файл digits.txt по возрастанию в файл sortedFile.txt
 
--- TsFT/src/academit/artamonov2017/tsft/Main.java
+++ TsFT/src/academit/artamonov2017/tsft/Main.java
@@ -16,8 +16,8 @@ public class Main {
 
         if (typeOfSorting.equals("-h")) {
             System.out.println("Программа должна запускаться с параметрами:");
-            System.out.println("a - сотрировка по возрастанию");
-            System.out.println("b - сортировка по убыванию");
+            System.out.println("-a - сотрировка по возрастанию");
+            System.out.println("-b - сортировка по убыванию");
             System.out.println("file.txt - название файла с расширением.");
             System.out.println("Отсортированный файл будет находиться в файле sortedFile.txt");
             System.out.println("(будет перезаписан, если уже существует.)");
@@ -28,14 +28,14 @@ public class Main {
         ArrayList<String> arrayFromFile = ReadSortWrite.readToArray(args[1]);
 
 
-        if (typeOfSorting.equals("a")) {
+        if (typeOfSorting.equals("-a")) {
             ReadSortWrite.sortArrayAscending(arrayFromFile);
             System.out.println("Файл отсортирован по возрастанию.");
-        } else if (typeOfSorting.equals("d")) {
+        } else if (typeOfSorting.equals("-d")) {
             ReadSortWrite.sortArrayDescending(arrayFromFile);
             System.out.println("Файл отсортирован по убыванию.");
         } else {
-            System.out.println("Вы должны выбрать \"a\" или \"d\", файл не отсортирован.");
+            System.out.println("Вы должны выбрать \"-a\" или \"-d\", файл не отсортирован.");
             return;
         }
 
