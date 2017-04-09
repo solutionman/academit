1ffca4f589853275d11c9a4352f88b819f847e2f
solutionman <solutionman@inbox.ru>
2017-04-09 21:57:06 +0700

    working on converting

--- CSV/src/CSVtoHTML.java
+++ CSV/src/CSVtoHTML.java
@@ -1,11 +1,11 @@
 import java.io.*;
 
 public class CSVtoHTML {
-    public static void convertCSVHTML (String filenameCSV, String filenameHTML){
+    public static void convertCSVHTML(String filenameCSV, String filenameHTML) {
 
-        try(
+        try (
                 BufferedReader in = new BufferedReader(new FileReader(filenameCSV));
-            PrintWriter out = new PrintWriter(filenameHTML)) {
+                PrintWriter out = new PrintWriter(filenameHTML)) {
             //BufferedReader in = new BufferedReader(new FileReader(filenameCSV));
             //PrintWriter out = new PrintWriter(filenameHTML);
 
@@ -19,17 +19,34 @@ public class CSVtoHTML {
             out.println("<meta charset=\"UTF-8\">");
             out.println("<head>");
             out.println("<body>");
+            out.println("<table border=\"1\">");
 
             String line;
-            while ((line = in.readLine()) != null){
-                String lineForHTML = line.replace("&", "&amp").replace("<", "&lt");
+            while ((line = in.readLine()) != null) {
+
+                out.println("<tr>");
+                out.println("<td>");
+                String lineForHTML = line.replace("&", "&amp").replace("<", "&lt").replace(">", "&gt");
+
+                String finalLineForHTML = "";
+                for (int i = 0; i < lineForHTML.length() - 1; ++i) {
+                    if (lineForHTML.charAt(i) == ',' && lineForHTML.charAt(i + 1) != '"') {
+                        lineForHTML = lineForHTML.substring(0, i) + "</td><td>" + lineForHTML.substring(i + 1);
+                    }
+                }
+
+                out.println(lineForHTML);
+
+                out.println("</td>");
+                out.println("</tr>");
             }
 
+            out.println("</table>");
             out.println("</body>");
             out.println("</html>");
-        } catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("No file");
-        } catch(IOException e){
+        } catch (IOException e) {
             System.out.println("IOException");
         }
     }
