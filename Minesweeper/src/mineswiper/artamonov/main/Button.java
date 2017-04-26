abe663ad4a6ff044b031258e2cfec68122aa2a67
lom <diskedge@yandex.ru>
2017-04-26 09:38:16 +0700

    switch to gridBagLayout

---
+++ Minesweeper/src/mineswiper/artamonov/main/Button.java
@@ -0,0 +1,15 @@
+package mineswiper.artamonov.main;
+
+public class Button {
+
+    String name;
+    int gridx;
+    int gridy;
+
+    public Button(String name, int gridx, int gridy){
+        this.name = name;
+        this.gridx = gridx;
+        this.gridy = gridy;
+    }
+
+}
--- Minesweeper/src/mineswiper/artamonov/main/Main.java
+++ Minesweeper/src/mineswiper/artamonov/main/Main.java
@@ -15,18 +15,67 @@ public class Main {
                 frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
                 frame.setVisible(true);
 
-                JPanel top = new JPanel();
-                frame.add(top, BorderLayout.CENTER );
+                JPanel panel = new JPanel(new GridBagLayout());
+                frame.getContentPane().add(panel);
+                GridBagConstraints c = new GridBagConstraints();
 
-                JPanel field = new JPanel();
-                frame.add(field, BorderLayout.CENTER);
+                JButton[] buttons = new JButton[100];
+                for (int i = 0; i < 100; ++i) {
+                    buttons[i] = new JButton(String.valueOf(i));
+                    //c.gridy = i;
+                    //c.gridy = i;
+                    //panel.add(buttons[i], c);
+                }
 
-                JButton newGame = new JButton("New Game");
-                field.add(newGame, BorderLayout.CENTER);
+                int k = 0;
+                for (int i = 0; i < 3; ++i) {
+                    for (int j = 0; j < 2; ++i) {
+                        c.gridx = i;
+                        c.gridy = j;
+                        panel.add(buttons[k]);
+                        ++k;
+                    }
+                }
 
-                JButton button = new JButton("Click me");
-                field.add(button, BorderLayout.CENTER);
+                Button testButton = new Button("testButton", 3, 4);
+                Button testButton2 = new Button("testButton2", 3, 5);
 
+                /*
+                JButton button1 = new JButton("1");
+                c.gridx = 0;
+                c.gridy = 0;
+                panel.add(button1, c);
+
+                JButton button2 = new JButton("2");
+                c.gridx = 1;
+                c.gridy = 0;
+                panel.add(button2, c);
+
+                JButton button3 = new JButton("3");
+                c.gridx = 2;
+                c.gridy = 0;
+                panel.add(button3, c);
+
+                JButton button4 = new JButton("4");
+                c.gridx = 3;
+                c.gridy = 0;
+                panel.add(button4, c);
+
+                JButton button5 = new JButton("5");
+                c.gridx = 4;
+                c.gridy = 0;
+                panel.add(button5, c);
+
+                JButton button6 = new JButton("6");
+                c.gridx = 0;
+                c.gridy = 1;
+                panel.add(button6, c);
+
+                JButton button7 = new JButton("7");
+                c.gridx = 0;
+                c.gridy = 2;
+                panel.add(button7, c);
+                */
             }
         });
 
