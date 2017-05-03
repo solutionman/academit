fe2b6192ffdb4f3c96690cc99be23c4b0ec7ddea
lom <diskedge@yandex.ru>
2017-05-03 09:37:06 +0700

    icons now shown

--- Minesweeper/src/mineswiper/artamonov/main/Main.java
+++ Minesweeper/src/mineswiper/artamonov/main/Main.java
@@ -13,7 +13,7 @@ public class Main {
             @Override
             public void run() {
                 JFrame frame = new JFrame("Mineswiper");
-                frame.setSize(500, 600);
+                frame.setSize(800, 500);
                 frame.setResizable(false);
                 frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
                 frame.setVisible(true);
@@ -23,84 +23,98 @@ public class Main {
                 GridBagConstraints c = new GridBagConstraints();
 
 
-
-                JButton[] buttons = new JButton[100];
-                for (int i = 0; i < 100; ++i) {
-                    buttons[i] = new JButton(" ");
-                }
-
-                int[] buttonsCondition = new int[100];
-                for(int i = 0; i < 100; ++i){
-                    buttonsCondition[i] = ThreadLocalRandom.current().nextInt(0, 2);
-                    System.out.printf("Button %d = %d", i, buttonsCondition[i]);
-                    System.out.println();
-                }
-
-                int k = 0;
-                for (int i = 0; i < 10; ++i) {
-                    for (int j = 0; j < 10; ++j) {
-                        c.gridx = i;
-                        c.gridy = j;
-                        c.anchor = GridBagConstraints.CENTER;
-                        //c.weighty = 1;
-                        panel.add(buttons[k], c);
-                        ++k;
-                    }
-                }
-
                 try {
                     Icon blowed = new ImageIcon("blowed.jpg");
                     Icon bomb = new ImageIcon("bomb.jpg");
+                    Icon empty = new ImageIcon("empty.jpg");
 
-                for(int i = 0; i < 100; ++i) {
-                    final Integer j = i;
-                    buttons[i].addActionListener(new ActionListener() {
+                    JButton[] buttons = new JButton[100];
+                    for (int i = 0; i < 100; ++i) {
+                        buttons[i] = new JButton();
+                        buttons[i].setIcon(empty);
+                    }
+
+                    int[] buttonsCondition = new int[100];
+                    for(int i = 0; i < 100; ++i){
+                        buttonsCondition[i] = ThreadLocalRandom.current().nextInt(0, 2);
+                        System.out.printf("Button %d = %d", i, buttonsCondition[i]);
+                        System.out.println();
+                    }
+
+                    int k = 0;
+                    for (int i = 0; i < 10; ++i) {
+                        for (int j = 0; j < 10; ++j) {
+                            c.gridx = i;
+                            c.gridy = j;
+                            c.anchor = GridBagConstraints.CENTER;
+                            //c.weighty = 1;
+                            panel.add(buttons[k], c);
+                            ++k;
+                        }
+                    }
+
+
+
+                    for(int i = 0; i < 100; ++i) {
+                        final Integer j = i;
+                        buttons[i].addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                //JOptionPane.showMessageDialog(frame, "you press me");
+                                //Integer j = i;
+                                System.out.printf("button %d pressed, condition: ", j);
+                                System.out.println(buttonsCondition[j]);
+                                //++j;
+                                if(buttonsCondition[j] == 1){
+                                    for(int i = 0; i < 100; ++i){
+                                        buttons[i].setEnabled(false);
+                                        if(buttonsCondition[i] == 1){
+                                            buttons[i].setIcon(bomb);
+                                        }
+                                        if(buttonsCondition[i] == 0){
+                                            buttons[i].setIcon(empty);
+                                        }
+                                    }
+                                    buttons[j].setIcon(blowed);
+                                    JOptionPane.showMessageDialog(frame, "Game Over");
+
+                                } else {
+                                    buttons[j].setEnabled(false);
+                                }
+                            }
+                        });
+                    }
+
+
+
+                    JButton newGame = new JButton("New Game");
+                    c.ipady  = 0;
+                    c.gridx = 10;
+                    c.gridy = 10;
+                    //c.weighty = 0.3;
+                    c.anchor = GridBagConstraints.NORTH;
+                    panel.add(newGame, c);
+
+                    newGame.addActionListener(new ActionListener() {
                         @Override
                         public void actionPerformed(ActionEvent e) {
-                            //JOptionPane.showMessageDialog(frame, "you press me");
-                            //Integer j = i;
-                            System.out.printf("button %d pressed, condition: ", j);
-                            System.out.println(buttonsCondition[j]);
-                            //++j;
-                            if(buttonsCondition[j] == 1){
-                                for(int i = 0; i < 100; ++i){
-                                    buttons[i].setEnabled(false);
-                                }
-                                buttons[j].setIcon(blowed);
-                                JOptionPane.showMessageDialog(frame, "Game Over");
+                            for(int i = 0; i < 100 ; ++i){
+                                buttons[i].setEnabled(true);
+                                buttons[i].setIcon(empty);
+                            }
+                            for(int i = 0; i < 100; ++i){
+                                buttonsCondition[i] = ThreadLocalRandom.current().nextInt(0, 2);
+                                System.out.printf("Button %d = %d", i, buttonsCondition[i]);
 
-                            } else {
-                                buttons[j].setEnabled(false);
+                                System.out.println();
                             }
                         }
                     });
-                }
 
                 } catch(Exception e){
                     System.out.println("No image");
                 }
 
-                JButton newGame = new JButton("New Game");
-                c.ipady  = 0;
-                c.gridx = 10;
-                c.gridy = 10;
-                //c.weighty = 0.3;
-                c.anchor = GridBagConstraints.NORTH;
-                panel.add(newGame, c);
-
-                newGame.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        for(int i = 0; i < 100 ; ++i){
-                            buttons[i].setEnabled(true);
-                        }
-                        for(int i = 0; i < 100; ++i){
-                            buttonsCondition[i] = ThreadLocalRandom.current().nextInt(0, 2);
-                            System.out.printf("Button %d = %d", i, buttonsCondition[i]);
-                            System.out.println();
-                        }
-                    }
-                });
             }
         });
 
