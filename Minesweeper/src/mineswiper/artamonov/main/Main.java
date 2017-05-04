2536e1286d35af02c6ded6fcacb50bb72be62c9e
lom <diskedge@yandex.ru>
2017-05-04 11:46:19 +0700

    working on counting bombs

--- Minesweeper/src/mineswiper/artamonov/main/Main.java
+++ Minesweeper/src/mineswiper/artamonov/main/Main.java
@@ -27,6 +27,14 @@ public class Main {
                     Icon blowed = new ImageIcon("blowed.jpg");
                     Icon bomb = new ImageIcon("bomb.jpg");
                     Icon empty = new ImageIcon("empty.jpg");
+                    Icon one = new ImageIcon("one.jpg");
+                    Icon two = new ImageIcon("two.jpg");
+                    Icon three = new ImageIcon("three.jpg");
+                    Icon four = new ImageIcon("four.jpg");
+                    Icon five = new ImageIcon("five.jpg");
+                    Icon six = new ImageIcon("six.jpg");
+                    Icon seven = new ImageIcon("seven.jpg");
+                    Icon eight = new ImageIcon("eight.jpg");
 
                     JButton[] buttons = new JButton[100];
                     for (int i = 0; i < 100; ++i) {
@@ -35,7 +43,7 @@ public class Main {
                     }
 
                     int[] buttonsCondition = new int[100];
-                    for(int i = 0; i < 100; ++i){
+                    for (int i = 0; i < 100; ++i) {
                         buttonsCondition[i] = ThreadLocalRandom.current().nextInt(0, 2);
                         System.out.printf("Button %d = %d", i, buttonsCondition[i]);
                         System.out.println();
@@ -54,8 +62,7 @@ public class Main {
                     }
 
 
-
-                    for(int i = 0; i < 100; ++i) {
+                    for (int i = 0; i < 100; ++i) {
                         final Integer j = i;
                         buttons[i].addActionListener(new ActionListener() {
                             @Override
@@ -65,14 +72,64 @@ public class Main {
                                 System.out.printf("button %d pressed, condition: ", j);
                                 System.out.println(buttonsCondition[j]);
                                 //++j;
-                                if(buttonsCondition[j] == 1){
-                                    for(int i = 0; i < 100; ++i){
+                                if (buttonsCondition[j] == 1) {
+                                    for (int i = 0; i < 100; ++i) {
                                         buttons[i].setEnabled(false);
-                                        if(buttonsCondition[i] == 1){
+                                        if (buttonsCondition[i] == 1) {
                                             buttons[i].setIcon(bomb);
                                         }
-                                        if(buttonsCondition[i] == 0){
-                                            buttons[i].setIcon(empty);
+
+                                        //   -11  -1    +9
+                                        //   -10 button +10
+                                        //   -9   +1    +11
+
+                                        if (buttonsCondition[i] == 0) {
+                                            int count = 0;
+                                            if (buttonsCondition[i - 1] == 1) {
+                                                ++count;
+                                            }
+                                            if (buttonsCondition[i + 1] == 1) {
+                                                ++count;
+                                            }
+                                            if (buttonsCondition[i + 10] == 1) {
+                                                ++count;
+                                            }
+                                            if (buttonsCondition[i - 10] == 1){
+                                                ++count;
+                                            }
+                                            if (buttonsCondition[i + 9] == 1){
+                                                ++count;
+                                            }
+                                            if (buttonsCondition[i - 11] == 1){
+                                                ++count;
+                                            }
+                                            if (buttonsCondition[i + 11] == 1){
+                                                ++count;
+                                            }
+                                            if (buttonsCondition[i - 9] == 1){
+                                                ++count;
+                                            }
+
+                                            if(count == 1){
+                                                buttons[i].setIcon(one);
+                                            } else if (count == 2){
+                                                buttons[i].setIcon(two);
+                                            } else if (count == 3){
+                                                buttons[i].setIcon(three);
+                                            } else if (count == 4){
+                                                buttons[i].setIcon(four);
+                                            } else if (count == 5){
+                                                buttons[i].setIcon(five);
+                                            } else if (count == 6){
+                                                buttons[i].setIcon(six);
+                                            } else if (count == 7){
+                                                buttons[i].setIcon(seven);
+                                            } else if (count ==8){
+                                                buttons[i].setIcon(eight);
+                                            } else {
+                                                buttons[i].setIcon(empty);
+                                            }
+                                            
                                         }
                                     }
                                     buttons[j].setIcon(blowed);
@@ -86,9 +143,8 @@ public class Main {
                     }
 
 
-
                     JButton newGame = new JButton("New Game");
-                    c.ipady  = 0;
+                    c.ipady = 0;
                     c.gridx = 10;
                     c.gridy = 10;
                     //c.weighty = 0.3;
@@ -98,11 +154,11 @@ public class Main {
                     newGame.addActionListener(new ActionListener() {
                         @Override
                         public void actionPerformed(ActionEvent e) {
-                            for(int i = 0; i < 100 ; ++i){
+                            for (int i = 0; i < 100; ++i) {
                                 buttons[i].setEnabled(true);
                                 buttons[i].setIcon(empty);
                             }
-                            for(int i = 0; i < 100; ++i){
+                            for (int i = 0; i < 100; ++i) {
                                 buttonsCondition[i] = ThreadLocalRandom.current().nextInt(0, 2);
                                 System.out.printf("Button %d = %d", i, buttonsCondition[i]);
 
@@ -111,7 +167,7 @@ public class Main {
                         }
                     });
 
-                } catch(Exception e){
+                } catch (Exception e) {
                     System.out.println("No image");
                 }
 
